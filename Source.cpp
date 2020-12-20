88de5596bc857e17a5e9281af8d19b28aaca9b76
AminaYehia <aminasaad1218@gmail.com>
2020-12-20 08:18:34 +0200

    fcommit

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int number1;
+	int number2;
+	int sum;
+	cout<<"please enter two numbers"<<endl;
+	cin>>number1>> number2;
+	sum=number1+number2;
+	cout<<"the sum is"<<sum<<endl;
+	system ("pause");
+
+}
